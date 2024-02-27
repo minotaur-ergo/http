f101a1eb90cfe738aba92d796470968be49efec9
vorujack <vorujack@gmail.com>
2024-02-27 10:36:57 +0330

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorCommunityHttp',
+      name: 'capacitorHttp',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
